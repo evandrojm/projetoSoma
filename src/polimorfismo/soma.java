b039ad09d9f695153174a6d55c1f79e5a16ba33d
Evandro Moreira <evandrocomputacao2017@gmail.com>
2019-07-09 11:47:10 -0300

    Commit Get e Set

--- src/polimorfismo/soma.java
+++ src/polimorfismo/soma.java
@@ -3,5 +3,46 @@ package polimorfismo;
 
 public class soma {
 
-    int num1, num2;
+    private int num1;
+    private int num2;
+    private int num3;
+    private int num4;
+
+    public soma() {
+    }
+
+    
+    
+    public int getNum1() {
+        return num1;
+    }
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getNum3() {
+        return num3;
+    }
+    public void setNum3(int num3) {
+        this.num3 = num3;
+    }
+
+    
+    public int getNum4() {
+        return num4;
+    }
+
+    /**
+     * @param num4 the num4 to set
+     */
+    public void setNum4(int num4) {
+        this.num4 = num4;
+    }    
 }
