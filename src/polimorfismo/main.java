85b0e26c401d1284fc74f482472facb06d5393c9
evandrojm <52708204+evandrojm@users.noreply.github.com>
2019-07-09 15:42:38 -0300

    Update main.java

--- src/polimorfismo/main.java
+++ src/polimorfismo/main.java
@@ -4,17 +4,19 @@ package polimorfismo;
 public class main extends soma{
 
     public static void main(String[] args) {
+       
+        int total;
         
         // Instanciar Soma 
         soma s = new soma();  
         
-        soma = s.soma(6, 4);
-        System.out.println("Soma de Dois Números: " + soma);
+        total = s.soma(6, 4);
+        System.out.println("Soma de Dois Números: " + total);
               
-        soma = s.soma(7, 1, 34);
-        System.out.println("Soma de Três Números: " + soma);
+        total = s.soma(7, 1, 34);
+        System.out.println("Soma de Três Números: " + total);
         
-        soma = s.soma(64, 9, 2, 9);
-        System.out.println("Soma de Quatro Números: " + soma);
+        total = s.soma(64, 9, 2, 9);
+        System.out.println("Soma de Quatro Números: " + total);
     }
 }
