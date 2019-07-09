3ac7bc4c028710726ad69eee318b4bbbcbecb48d
Evandro Moreira <evandrocomputacao2017@gmail.com>
2019-07-09 13:45:44 -0300

    Terminando a Main

--- src/polimorfismo/main.java
+++ src/polimorfismo/main.java
@@ -1,9 +1,20 @@
 
 package polimorfismo;
 
-public class main {
+public class main extends soma{
 
     public static void main(String[] args) {
+        int soma;
         
+        soma a = new soma();        
+        soma = a.soma(6, 4);
+        System.err.println("Soma de Dois Números: " + soma);
+        soma b = new soma();        
+        soma = b.soma(7, 1, 34);
+        System.err.println("Soma de Três Números: " + soma);
+        
+        soma c = new soma();
+        soma = c.soma(64, 9, 2, 8);
+        System.err.println("Soma de Quatro Números: " + soma);
     }
 }
