04ab58ca80c13b53b5f2617e6f58e21b490a1217
Evandro Moreira <evandrocomputacao2017@gmail.com>
2019-07-09 13:49:20 -0300

    Terminando classe soma

--- src/polimorfismo/main.java
+++ src/polimorfismo/main.java
@@ -8,13 +8,13 @@ public class main extends soma{
         
         soma a = new soma();        
         soma = a.soma(6, 4);
-        System.err.println("Soma de Dois Números: " + soma);
+        System.out.println("Soma de Dois Números: " + soma);
         soma b = new soma();        
         soma = b.soma(7, 1, 34);
-        System.err.println("Soma de Três Números: " + soma);
+        System.out.println("Soma de Três Números: " + soma);
         
         soma c = new soma();
-        soma = c.soma(64, 9, 2, 8);
-        System.err.println("Soma de Quatro Números: " + soma);
+        soma = c.soma(64, 9, 2, 9);
+        System.out.println("Soma de Quatro Números: " + soma);
     }
 }
